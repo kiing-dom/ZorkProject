794b79edaf7f6e469cfcfac0a3a06600372ef3bf
Dominion Gbadamosi <74149150+kiing-dom@users.noreply.github.com>
2022-03-26 15:01:50 +0000

    addition of a weapon class
    addition of a weapon class as an extension of the item class

---
+++ Weapon.cpp
@@ -0,0 +1,8 @@
+#include "Weapon.h"
+
+Weapon::Weapon(string description, int weightGrams, float value,
+               float damage, int durability) : Item(description, weightGrams, value){
+    this->damage = damage;
+    this->durability = durability;
+
+}
